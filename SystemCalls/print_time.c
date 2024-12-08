f849e99ce4491da1487a58563e9db95494db5ce7
charanmakkar <charanmakkar2@gmail.com>
2024-12-09 00:47:04 +0530

    update

--- SystemCalls/print_time.c
+++ SystemCalls/print_time.c
@@ -10,13 +10,13 @@ int main() {
 	long milliseconds;
 
 	//TODO 1: Populate the timeval structure with gettimeofday
-
+	gettimeofday(&tv, NULL);
 
 	//TODO 2: Use localtime to get struct tm
-
+	ptm = localtime(&tv.tv_sec);
 
 	//TODO 3: Use strftime to get the time in desired format
-
+	strftime(time_string, sizeof(time_string), "%Y-%m-%d %H:%M:%S", ptm);
 
 	milliseconds = tv.tv_usec / 1000;
 	printf("%s.%03ld\n", time_string, milliseconds);
